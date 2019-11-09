a0f63f29bffde35aaa99b9e53aac10c0e519cd84
GondaKid <53237849+GondaKid@users.noreply.github.com>
2019-11-09 11:40:44 +0700

    Create Utilities.h
    MakeButton_NameGame_DoneCharater_and_Screen

---
+++ Utilities.h
@@ -0,0 +1,30 @@
+﻿#pragma once
+#include "Map_and_Characters.h"
+
+class Option	//Class các sự lựa chọn
+{
+public:
+	Coord Pos;	//Vị trí sự lựa chọn
+	string data;	//Tên sự lựa chọn
+public:
+	Option(const int _x = WidthCenter, const int _y = HeightCenter, const string _data = " ");
+	~Option();
+
+};
+
+class Menu
+{
+private:
+	vector<Option> list_Option;	//List sự lựa chọn
+	string menu_Frames;	//Khung menu
+	string menu_guide;	//Hướng dẫn sử dụng menu
+public:
+	Menu();	//Khởi tạo menu
+	~Menu();
+	void Print_GameName();	//In ra tên game
+	void Print();	//In ra menu
+	void Cursor_On(int current);	//Đặt con trỏ tại vị trí đang lựa chọn
+	void del_Old_Cur(int old_Pos);	//Xóa con trỏ cũ
+	int Player_Choose();	//Lựa chọn của người chơi
+};
+
